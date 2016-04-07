e2f3a2046e2cea78f6a44b45dc6dce0e4333d020
hassanalubeidi <hassanalubeidi1996@gmail.com>
2016-04-07 17:32:42 +0100

    added grouping for problem feed. still requires some bug fixes

--- public/scripts/directives/problem-feed/problem-feed.js
+++ public/scripts/directives/problem-feed/problem-feed.js
@@ -5,95 +5,151 @@
  * # topNavbar
  */
 angular.module('smartLearnIoApp')
-  .directive('problemFeed', function () {
-    return {
-      templateUrl: 'scripts/directives/problem-feed/problem-feed.html',
-      restrict: 'E'
-    };
-  })
-  .controller('problemFeedController', ['$scope', '$http', '$timeout', 'JsonAPI', 'Problem', 'Topic', 'apiBaseUrl',
-   function($scope, $http, $timeout, JsonAPI, Problem, Topic, apiBaseUrl) {
-      // Load Problems
-      Problem.where({'user-id': $scope.user.id}).then(function(response) {
-          $scope.problems = response.data.data.map(function(problem) {
-	      // TODO: Fix this
-              problem.attributes.id = problem.id;
-              return problem.attributes;
-          });
-	  console.log("Problems fetched for user id: " + $scope.user.id);
-	  console.log($scope.problems);
-      })
-      // Create Problem
-      $scope.createProblem = function(newProblem) {
-          // Find topic by name
-	  // Use index 1, because of weird array issue
-          var topic_name = '"' + $(".topic.search").search("get value") + '"';
-          console.log("The topic name from the search is: ");
-	  console.log(topic_name);
+    .directive('problemFeed', function() {
+        return {
+            templateUrl: 'scripts/directives/problem-feed/problem-feed.html',
+            restrict: 'E'
+        };
+    })
+    .controller('problemFeedController', ['$scope', '$http', '$timeout', 'JsonAPI', 'Problem', 'Topic', 'apiBaseUrl',
+        function($scope, $http, $timeout, JsonAPI, Problem, Topic, apiBaseUrl) {
+            // Load Problems
+            Problem.where({'user-id': $scope.user.id}).then(function(response) {
+            $scope.problems = response.data.data.map(function(problem) {
+                problem.attributes.id = problem.id
+                return problem.attributes
+            })
+            var problemGroups = []
+            var problemGroup = {all: [], minimized: [], loading: false}
+            var lastTestPaper
+            $scope.problems.map(function(problem, index) {
+              if(problem['test-paper'] != undefined) {
+                  if (lastTestPaper != problem['test-paper'].title) {
+                      if (problemGroup.all.length > 0) {
+                          problemGroups.push(problemGroup)
+                      }
+                      problemGroup = {all: [problem], minimized: [problem], loading: false}
+                  }else {
+                      problemGroup.all.push(problem)
+                      if (problemGroup.minimized.length < 4) {
+                          problemGroup.minimized.push(problem)
+                      }
+                      if (index == $scope.problems.length -1) {
+                          problemGroups.push(problemGroup)
+                      }
+                  }
+              }else {
+                  
+                  if (problemGroup.all.length > 0) {
+                      problemGroups.push(problemGroup)
+                  }
+                  problemGroup = {all: [], minimized: [], loading: false}
+                  problemGroups.push({all: [problem], minimized: [problem], loading: false})
+              }
+                
+                
+              if(problem['test-paper'] != null ) {
+                  lastTestPaper = problem['test-paper'].title
+              }else {
+                  lastTestPaper = null
+              }
+            })
+            console.log(problemGroup)
+            $scope.problemGroups = problemGroups
+        })
+      
+      $scope.loadMoreProblems = function(problemGroup) {
+          $timeout(function() {
+              problemGroup.minimized = problemGroup.all
+              problemGroup.loading = false; 
+          }, 300)
+          problemGroup.loading = true; 
+          
+      }
 
-	  $http.get(apiBaseUrl + "/topics?filter[title]=" + topic_name).then(function(response) {
 
-		console.log("Response: ");
-		console.log(response);
+                // Create Problem
+            $scope.createProblem = function(newProblem) {
+                // Find topic by name
+                // Use index 1, because of weird array issue
+                var topic_name = '"' + $(".topic.search").search("get value") + '"';
+                console.log("The topic name from the search is: ");
+                console.log(topic_name);
 
-		var topic_id = response.data.data[0].id;
-                console.log("Topic id: " + topic_id);
+                $http.get(apiBaseUrl + "/topics?filter[title]=" + topic_name).then(function(response) {
 
-		var payload = {
-                    "data": {
-                        "type": "problems",
-                        "attributes": {
-			    "user-id": $scope.user.id,
-                            "topic-id": topic_id,
-                            "what-went-wrong": newProblem.what_went_wrong
+                    console.log("Response: ");
+                    console.log(response);
+
+                    var topic_id = response.data.data[0].id;
+                    console.log("Topic id: " + topic_id);
+
+                    var payload = {
+                        "data": {
+                            "type": "problems",
+                            "attributes": {
+                                "user-id": $scope.user.id,
+                                "topic-id": topic_id,
+                                "what-went-wrong": newProblem.what_went_wrong
+                            }
                         }
                     }
-            	}
 
-		$http({
-                    url: apiBaseUrl + '/problems',
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/vnd.api+json' },
-                    data: payload
-		}).then(function(problemResponse) {
-			// TODO: This is an ugly fix
-			//problemResponse.data.data.attributes.id = $scope.problems[$scope.problems.length - 1].id + 1;
-			$scope.problems.push(problemResponse.data.data.attributes);
-		});
-		//payload.data.attributes.id = $scope.problems[$scope.problems.length - 1].id + 1
-		// TODO: This should be more elegant
-		//payload.data.attributes.topic = {"title": topic_name};
-		//$scope.problems.push(payload.data.attributes);
+                    $http({
+                        url: apiBaseUrl + '/problems',
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/vnd.api+json'
+                        },
+                        data: payload
+                    }).then(function(problemResponse) {
+                        // TODO: This is an ugly fix
+                        //problemResponse.data.data.attributes.id = $scope.problems[$scope.problems.length - 1].id + 1;
+                        $scope.problems.push(problemResponse.data.data.attributes);
+                    });
+                    //payload.data.attributes.id = $scope.problems[$scope.problems.length - 1].id + 1
+                    // TODO: This should be more elegant
+                    //payload.data.attributes.topic = {"title": topic_name};
+                    //$scope.problems.push(payload.data.attributes);
 
-          });
+                });
 
-	};
+            };
 
-      // Delete Problem from Problems
-      $scope.removeProblem = function(array, index){
-          Problem.delete(array[index].id)
-          array.splice(index, 1);
-      }
-      // Get topics for problem-feed search bar
-      $http.get(apiBaseUrl + "/topics").then(function(response) {
+            // Delete Problem from Problems
+            $scope.removeProblem = function(array, index, problemGroupIndex, problemGroups){
+                  console.log(array)
+                  console.log(array[index].id)
+                  Problem.delete(array[index].id)
+                  
+                  array.splice(index, 1);
+                  if (problemGroups[problemGroupIndex].all.length == 1) { //delete entire group if only contains one problem 
+                      problemGroups[problemGroupIndex] = null
+                  }else { //delete only one problem from the group if group has >1 problems
+                      // TODO
+                  }
+              }
+                // Get topics for problem-feed search bar
+            $http.get(apiBaseUrl + "/topics").then(function(response) {
 
-          $scope.topics = JsonAPI.pluckAttributes(response.data.data);
+                $scope.topics = JsonAPI.pluckAttributes(response.data.data);
 
-	  console.log("Topics received");
-	  console.log($scope.topics);
+                console.log("Topics received");
+                console.log($scope.topics);
 
-          $('.ui.search')
-            .search({
-                source: $scope.topics
-           });
+                $('.ui.search')
+                    .search({
+                        source: $scope.topics
+                    });
 
-      });
-      // Initialize Semantic UI search with topics
-      /*$timeout(function() {
-           $('.ui.search')
-            .search({
-                source: $scope.topics
             });
-      }, 1000)*/
-     
-  }]);
+            // Initialize Semantic UI search with topics
+            /*$timeout(function() {
+                 $('.ui.search')
+                  .search({
+                      source: $scope.topics
+                  });
+            }, 1000)*/
+
+        }
+    ]);
